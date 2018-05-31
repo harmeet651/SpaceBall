4f4bc457740a06ad79999b788febc381c1330f62
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-05-30 21:29:12 -0700

    commit two
    slow speed added

--- Assets/Scripts/cameraMove.cs
+++ Assets/Scripts/cameraMove.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class cameraMove : MonoBehaviour {
 
+    public KeyCode moveSlow;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +13,10 @@ public class cameraMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
+		GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 4);
+        if(Input.GetKey(moveSlow))
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0.5f);
+        }
     }
 }
--- Assets/Scripts/shpereMove.cs
+++ Assets/Scripts/shpereMove.cs
@@ -4,13 +4,85 @@ using UnityEngine;
 
 public class shpereMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public KeyCode moveL;
+    public KeyCode moveR;
+    public KeyCode moveSlow;
+    public float horizVel = 0;
+    public int laneNum = 2;
+    public string controlLocked = "n";
+    Vector3 start, BackToStart;
+
+    void Start () {
+        //start = transform.position;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 5);
-	}
+        GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+        if (Input.GetKeyDown(moveL) && (laneNum > 1) && (controlLocked == "n"))
+        {
+            horizVel = -2;
+            StartCoroutine(stopSlide());
+            laneNum = laneNum - 1;
+            controlLocked = "y";
+        }
+        if (Input.GetKeyDown(moveR) && (laneNum < 3) && (controlLocked == "n"))
+        {
+            horizVel = 2;
+            StartCoroutine(stopSlide());
+            laneNum = laneNum + 1;
+            controlLocked = "y";
+        }
+        if(Input.GetKey(moveSlow) && controlLocked=="n")
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 0.5f);
+        }
+    }
+
+     IEnumerator stopSlide()
+{
+    yield return new WaitForSeconds(.5f);
+    horizVel = 0;
+    controlLocked = "n";
+}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag=="goBack")
+        {
+            transform.position = new Vector3(1, 0.8f, 5);
+        }
+        if (other.gameObject.tag == "goBackLeft")
+        {
+            transform.position = new Vector3(0, 0.8f, 5);
+        }
+        if (other.gameObject.tag == "goBackRight")
+        {
+            transform.position = new Vector3(2, 0.8f, 5);
+        }
+        if (other.gameObject.tag == "set2middle")
+        {
+            transform.position = new Vector3(1, 0.8f, 18);
+        }
+        if (other.gameObject.tag == "set2left")
+        {
+            transform.position = new Vector3(0, 0.8f, 18);
+        }
+        if (other.gameObject.tag == "set2right")
+        {
+            transform.position = new Vector3(2, 0.8f, 18);
+        }
+        if(other.gameObject.tag=="enterPipe2")
+        {
+            GetComponent<Rigidbody>().velocity= new Vector3(horizVel, 0, 80);
+        }
+        if (other.gameObject.tag == "exitPipe2")
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(horizVel, 0, 4);
+        }
+    }
 }
