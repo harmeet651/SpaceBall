69f7a56a0a4cfb8d3e5be9ce0c8e0be42a110dc8
Arpit Sharma <arpitshrm07@gmail.com>
2018-06-28 21:19:24 -0700

    Harry's Particle Effect
    Push from Harry for the particle effect added in the first lane. 
    The tilemanager now is positioned at Z=20 and the particlePrefab is hard-coded in unity scene inspector at z=0.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
 // GameController controls the overall aspect of the game
 // For example, generating tiles, obstacles should be handled here
@@ -15,22 +15,14 @@ public class GameController : MonoBehaviour
 
     private GameObject player;
     private PlayerController playerController;
-    private ScoreManager scoreManager;
-    private NotificationController notificationController; 
+    private NotificationController notificationController;
 
     public int numLanes = 5;                   // number of lanes
 
     //Swipe manager variables.
-    private bool hold = false;
+    private bool isTouchInHold = false;
     private float touchDuration = 0f, scrMid, TouchStart;
-
     public float tapDuration;
-
-    public Canvas tutorialCanvas;
-    public GameObject leftInstructionSlide;
-    public GameObject rightInstructionSlide;
-    public GameObject holdInstructionSlide;
-    public Text countDownText; 
 
 
     // Use this for initialization
@@ -38,37 +30,10 @@ public class GameController : MonoBehaviour
     {
         player = GameObject.FindWithTag("Player");
         playerController = player.GetComponent<PlayerController>();
-        scoreManager = player.GetComponent<ScoreManager>();
-        notificationController = GetComponent<NotificationController>(); 
+        notificationController = GetComponent<NotificationController>();
         scrMid = (float)Screen.width;
         scrMid = scrMid / 2;
         tapDuration = 0.165f;
-
-        StartCoroutine(InitialTutorial());
-    }
-    
-    IEnumerator InitialTutorial()
-    {
-        yield return new WaitForSeconds(3.0f);
-        leftInstructionSlide.SetActive(false);
-        rightInstructionSlide.SetActive(true);
-
-        yield return new WaitForSeconds(2.5f);
-        rightInstructionSlide.SetActive(false);
-        holdInstructionSlide.SetActive(true);
-
-        yield return new WaitForSeconds(3.0f);
-        holdInstructionSlide.SetActive(false);
-
-        yield return new WaitForSeconds(0.5f);
-        
-        for (int secondsLeft = 3; secondsLeft > 0; secondsLeft--)
-        {
-            countDownText.text = "" + secondsLeft; 
-            yield return new WaitForSeconds(0.8f); 
-        }
-
-        tutorialCanvas.enabled = false; 
     }
 
     // Update is called once per frame
@@ -85,7 +50,6 @@ public class GameController : MonoBehaviour
         if (Input.GetKey(moveSlow))
         {
             playerController.MoveSlow();
-            scoreManager.MoveSlow();
         }
 
         //Touch manager.
@@ -98,40 +62,42 @@ public class GameController : MonoBehaviour
             {
                 //Reset all related variables
                 touchDuration = 0;
-                hold = false;
+                isTouchInHold = false;
                 TouchStart = Time.time;
             }
+
             //Increment total touch duration
             touchDuration += Time.deltaTime;
 
-            if (hold == true)
+            if (isTouchInHold == true)
             {
                 playerController.MoveSlow();
-                scoreManager.MoveSlow();
             }
             else
             {
                 if (myTouch.phase == TouchPhase.Ended)
                 {
-                    hold = false;
+                    isTouchInHold = false;
                     if (myTouch.phase == TouchPhase.Ended && (Time.time - TouchStart) < tapDuration)
                     {
                         if (myTouch.position.x > scrMid)
+                        {
                             playerController.MoveRight();
+                        }
+
                         else
+                        {
                             playerController.MoveLeft();
-                        //Mark the swipe as handled.
+                        }
                     }
                 }
                 else if ((Time.time - TouchStart) > tapDuration)
                 {
-                    hold = true;
+                    isTouchInHold = true;
                     playerController.MoveSlow();
-                    scoreManager.MoveSlow();
-
                 }
             }
-        }//End of Touch Manager.
+        } //End of Touch Manager.
     }
 
     public float GetLaneCenterXPos(int laneNum)
@@ -141,13 +107,13 @@ public class GameController : MonoBehaviour
 
     public void GameOver()
     {
-        StartCoroutine(GameOverCoroutine()); 
+        StartCoroutine(GameOverCoroutine());
     }
 
     IEnumerator GameOverCoroutine()
     {
         notificationController.NotifyText("Game Over");
-        yield return new WaitForSeconds(3);
+        yield return new WaitForSeconds(0.1f);
         SceneManager.LoadScene("Scenes/MenuScene");
     }
 }
