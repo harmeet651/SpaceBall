fd0a2f3542f4422320df9888d86d8d83e8af7df4
Ye Joo Park <subwaymatch@gmail.com>
2018-07-03 17:09:25 -0700

    Tutorials and menu scene

---
+++ Assets/Scripts/NextButtonScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextButtonScript : MonoBehaviour {
+
+	
+	public void ChangeScene (string sceneName) {
+
+		Application.LoadLevel(sceneName);
+		
+	}
+	
+	
+}
