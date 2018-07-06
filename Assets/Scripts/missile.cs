b1fa758003af055d4188914ffd7d45f60ee692ec
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-07-06 01:28:55 -0700

    fixed some minor defects
    cleaner small code, mettalic missile, better particle effect

--- Assets/Scripts/missile.cs
+++ Assets/Scripts/missile.cs
@@ -7,121 +7,52 @@ public class missile : MonoBehaviour {
 
     float missileVelocity = 14;
     Rigidbody homingMissile;
-    float turn = 20;
-    int fuseDelay = 20;
-    GameObject missileMod;
-    public float speed;
-    public float verticalVelocity = 10f;
-    public bool isFlying = false;
-    public float altitude=0.5f;
-
-    //ParticleSystem SmokePrefab;
-    //AudioClip missileClip;
-
+    Vector3 m_EulerAngleVelocity;
+    //float turn = 20;
+    //int fuseDelay = 20;
+    //GameObject missileMod;
+    //public float speed;
     //public Transform target;
 
     void Start()
     {
-        //    Fire();
         homingMissile = GetComponent<Rigidbody>();
-        //homingMissile.detectCollisions = false;
-
+        //Set the axis the Rigidbody rotates in (100 in the z axis)
+        m_EulerAngleVelocity = new Vector3(0, 0, 1000);
     }
 
     void Update()
 
     {
+        //once destroyed, we cannot access rigidbody, so return
         if (homingMissile == null)
         {
             Destroy(homingMissile);
             return;
         }
-
-        //homingMissile.velocity = transform.forward * missileVelocity;
-        //if (transform.position.y < altitude)
-        //{
-        //    homingMissile.velocity = new Vector3(0, verticalVelocity, missileVelocity);
-        //}
-        //else
-        {
-            homingMissile.velocity = new Vector3(0, 0, missileVelocity);
-        }
-        //if (isFlying)
-        //{
-        //    Debug.Log("In flying");
-        //    homingMissile.velocity = new Vector3(0, verticalVelocity, missileVelocity * 3);
-        //}
-
-        //else
-        //{
-        //    Debug.Log("In other");
-        //    homingMissile.velocity = new Vector3(0, verticalVelocity, missileVelocity);
-        //}
-
-        //var targetRotation = Quaternion.LookRotation(target - transform.position);
-
-        //homingMissile.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, turn));
-
-        //Destroy(homingMissile, 4.0f);
+                homingMissile.velocity = transform.forward * missileVelocity;
+                //homingMissile.velocity = new Vector3(0, 0, missileVelocity);
+                //homingMissile.AddForce(0, 10, 0);
     }
-    //public void Fly()
-    //{
-    //    // Actual flying motion is done through missillleee
-    //    GetComponent<missillleee>().Fly();
-    //}
 
-    //void Fire()
+    //for missile rotation
+    void FixedUpdate()
+    {
+        Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * Time.deltaTime);
+        homingMissile.MoveRotation(homingMissile.rotation * deltaRotation);
+    }
 
-    //{
-
-    //    //Thread.Sleep(fuseDelay);
-    //    //AudioSource.PlayClipAtPoint(missileClip, transform.position);
-
-    //    var distance = Mathf.Infinity;
-
-    //    foreach (GameObject go in GameObject.FindGameObjectsWithTag("death"))
-    //    {
-    //        var diff = (go.transform.position - transform.position).sqrMagnitude;
-
-    //        if (diff < distance)
-    //        {
-    //            distance = diff;
-    //            target = go.transform;
-    //        }
-
-    //    }
-
-    //}
-
+    //destroy death trap and missile
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "death")
         {
-            Debug.Log("inside destroy missile and object");
-            
-            Destroy(collision.gameObject);
-            
+            Debug.Log("inside destroy missile and object");         
+            Destroy(collision.gameObject);           
             Destroy(this.gameObject);
-            //GetComponent<Rigidbody>().Sleep();
          }
-        //else
-        //{
-        //}
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        //    if(other.tag=="missileTrigger")
-        //    {
-        //        Debug.Log("Destroy needle");
-        //        homingMissile.detectCollisions = false;
-        //    }
-        //    if (other.tag == "missileTriggerDisable")
-        //    {
-        //        homingMissile.detectCollisions = true;
-
-        //    }
-    }
-
+    
 }
 
 
