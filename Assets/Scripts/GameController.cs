0a12926cd140b4fd882798ea51ae5f3b53e6cd10
harmeet651 <32997386+harmeet651@users.noreply.github.com>
2018-07-10 13:51:09 -0700

    Make music persist through scenes

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,162 +7,172 @@ using UnityEngine.UI;
 // GameController controls the overall aspect of the game
 // For example, generating tiles, obstacles should be handled here
 // Game over, restart actions should also be defined here
-public class GameController : MonoBehaviour
+namespace S3
 {
-    public KeyCode moveL;
-    public KeyCode moveR;
-    public KeyCode moveSlow;
-
-    private GameObject player;
-    private PlayerController playerController;
-    private NotificationController notificationController;
-
-    public int numLanes = 5;                   // number of lanes
-
-    //Swipe manager variables.
-    private bool isTouchInHold = false;
-    private float touchDuration = 0f, scrMid, TouchStart, wid, hig;
-    public float tapDuration;
-
-    public Button leftSlowButton, rightSlowButton;
-
-    private int slow;
-
-    // Use this for initialization
-    void Start()
+    public class GameController : MonoBehaviour
     {
-        player = GameObject.FindWithTag("Player");
-        playerController = player.GetComponent<PlayerController>();
-        notificationController = GetComponent<NotificationController>();
-        wid = (float)Screen.width;
-        hig = (float)Screen.height;
-        scrMid = wid / 2;
-        tapDuration = 0.165f;
-        slow = 0;
+        public GameObject gameOver;
 
-        // RectTransform rt = leftSlowButton.GetComponent<RectTransform>();
-        // rt.sizeDelta = new Vector2(wid/4, hig/7);
+        public KeyCode moveL;
+        public KeyCode moveR;
+        public KeyCode moveSlow;
 
-        //Button lt = leftSlowButton.GetComponent<Button>();
-        //Button rt = rightSlowButton.GetComponent<Button>();
+        private GameObject player;
+        private PlayerController playerController;
+        private NotificationController notificationController;
 
-        // lt.onClick.AddListener(moveslow);
-        // rt.onClick.AddListener(playerController.MoveSlow);
+        public int numLanes = 5;                   // number of lanes
 
-        //Debug.Log(rightSlowButton.transform.position.x);
-    }
+        //Swipe manager variables.
+        private bool isTouchInHold = false;
+        private float touchDuration = 0f, scrMid, TouchStart, wid, hig;
+        public float tapDuration;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(moveL))
+        public Button leftSlowButton, rightSlowButton;
+
+        private int slow;
+
+        // Use this for initialization
+        void Start()
         {
-            playerController.MoveLeft();
+            player = GameObject.FindWithTag("Player");
+            playerController = player.GetComponent<PlayerController>();
+            notificationController = GetComponent<NotificationController>();
+            wid = (float)Screen.width;
+            hig = (float)Screen.height;
+            scrMid = wid / 2;
+            tapDuration = 0.165f;
+            slow = 0;
+
+            // RectTransform rt = leftSlowButton.GetComponent<RectTransform>();
+            // rt.sizeDelta = new Vector2(wid/4, hig/7);
+
+            //Button lt = leftSlowButton.GetComponent<Button>();
+            //Button rt = rightSlowButton.GetComponent<Button>();
+
+            // lt.onClick.AddListener(moveslow);
+            // rt.onClick.AddListener(playerController.MoveSlow);
+
+            //Debug.Log(rightSlowButton.transform.position.x);
         }
-        if (Input.GetKeyDown(moveR))
-        {
-            playerController.MoveRight();
-        }
-        if (Input.GetKey(moveSlow))
-        {
-            playerController.MoveSlow();
-        }
 
-        //Touch manager.
-        if (Input.touchCount > 0)
+        // Update is called once per frame
+        void Update()
         {
-            //Get touch event by the first finger.
-            Touch myTouch = Input.GetTouch(0);
-            //Check If touch is just starting
-            if (myTouch.phase == TouchPhase.Began)
+            if (Input.GetKeyDown(moveL))
             {
-                //Reset all related variables
-                touchDuration = 0;
-                isTouchInHold = false;
-                TouchStart = Time.time;
-
-                // if(myTouch.position.x < (wid/4) || myTouch.position.x > (wid-(wid/4)) ){
-                // 	if(myTouch.position.y < (wid/7)){
-                // 		slow = 1;
-                // 	}
-                // }
-
-                if(slow != 1){
-                	if(myTouch.position.x > scrMid){
-                		playerController.MoveRight();
-                	}
-                	else{
-                		playerController.MoveLeft();
-                	}
-                }
+                playerController.MoveLeft();
+            }
+            if (Input.GetKeyDown(moveR))
+            {
+                playerController.MoveRight();
+            }
+            if (Input.GetKey(moveSlow))
+            {
+                playerController.MoveSlow();
             }
 
-            
+            //Touch manager.
+            if (Input.touchCount > 0)
+            {
+                //Get touch event by the first finger.
+                Touch myTouch = Input.GetTouch(0);
+                //Check If touch is just starting
+                if (myTouch.phase == TouchPhase.Began)
+                {
+                    //Reset all related variables
+                    touchDuration = 0;
+                    isTouchInHold = false;
+                    TouchStart = Time.time;
 
-            //Increment total touch duration
-            // touchDuration += Time.deltaTime;
+                    // if(myTouch.position.x < (wid/4) || myTouch.position.x > (wid-(wid/4)) ){
+                    // 	if(myTouch.position.y < (wid/7)){
+                    // 		slow = 1;
+                    // 	}
+                    // }
 
-            // if (isTouchInHold == true)
-            // {
-            //     playerController.MoveSlow();
-            // }
-            // else
-            // {
-            //     if (myTouch.phase == TouchPhase.Ended)
-            //     {
-            //         isTouchInHold = false;
-            //         if (myTouch.phase == TouchPhase.Ended && (Time.time - TouchStart) < tapDuration)
-            //         {
-            //             if (myTouch.position.x > scrMid)
-            //             {
-            //                 playerController.MoveRight();
-            //             }
+                    if (slow != 1) {
+                        if (myTouch.position.x > scrMid) {
+                            playerController.MoveRight();
+                        }
+                        else {
+                            playerController.MoveLeft();
+                        }
+                    }
+                }
 
-            //             else
-            //             {
-            //                 playerController.MoveLeft();
-            //             }
-            //         }
-            //     }
-            //     else if ((Time.time - TouchStart) > tapDuration)
-            //     {
-            //         isTouchInHold = true;
-            //         playerController.MoveSlow();
-            //     }
-            // }
-        } //End of Touch Manager.
 
-        if(slow == 1){
-        	playerController.MoveSlow();
+
+                //Increment total touch duration
+                // touchDuration += Time.deltaTime;
+
+                // if (isTouchInHold == true)
+                // {
+                //     playerController.MoveSlow();
+                // }
+                // else
+                // {
+                //     if (myTouch.phase == TouchPhase.Ended)
+                //     {
+                //         isTouchInHold = false;
+                //         if (myTouch.phase == TouchPhase.Ended && (Time.time - TouchStart) < tapDuration)
+                //         {
+                //             if (myTouch.position.x > scrMid)
+                //             {
+                //                 playerController.MoveRight();
+                //             }
+
+                //             else
+                //             {
+                //                 playerController.MoveLeft();
+                //             }
+                //         }
+                //     }
+                //     else if ((Time.time - TouchStart) > tapDuration)
+                //     {
+                //         isTouchInHold = true;
+                //         playerController.MoveSlow();
+                //     }
+                // }
+            } //End of Touch Manager.
+
+            if (slow == 1) {
+                playerController.MoveSlow();
+            }
         }
-    }
 
 
-   
-    public float GetLaneCenterXPos(int laneNum)
-    {
-        return (float)(laneNum - (numLanes / 2 + 1));
-    }
 
-    public void GameOver()
-    {
-        StartCoroutine(GameOverCoroutine());
-    }
+        public float GetLaneCenterXPos(int laneNum)
+        {
+            return (float)(laneNum - (numLanes / 2 + 1));
+        }
 
-    IEnumerator GameOverCoroutine()
-    {
-        notificationController.NotifyText("Game Over");
-        yield return new WaitForSeconds(0.1f);
-        SceneManager.LoadScene("Scenes/MenuScene");
-    }
+        public void GameOver()
+        {
+            StartCoroutine(GameOverCoroutine());
+        }
 
-    public void slowModeOn(){
-    	slow = 1;
-    	//Debug.Log("Slow button pressed!");
-    }
+        IEnumerator GameOverCoroutine()
+        {
+            notificationController.NotifyText("Game Over");
+            yield return new WaitForSeconds(0.1f);
+            SceneManager.LoadScene("Scenes/MenuScene");
+        }
 
-    public void slowModeOff(){
-    	slow = 0;
-    	//Debug.Log("Slow button pressed!");
+        public void slowModeOn() {
+            slow = 1;
+            //Debug.Log("Slow button pressed!");
+        }
+
+        public void slowModeOff() {
+            slow = 0;
+            //Debug.Log("Slow button pressed!");
+        }
+        private void OnEnable()
+        {
+            //SetInitialReferences();
+
+        }
     }
 }
