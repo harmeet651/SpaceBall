2bf7af64020bcf444d50adea2dbd174df7d70a40
Ye Joo Park <subwaymatch@gmail.com>
2018-07-10 09:17:51 -0700

    Fix merge issues

--- Assets/Scripts/NotificationController.cs
+++ Assets/Scripts/NotificationController.cs
@@ -7,17 +7,20 @@ public class NotificationController : MonoBehaviour
 {
     public Canvas notificationCanvas;
     public GameObject WellDoneCanvas;
-    public Image backgroundImage; 
+    public Image backgroundImage;
     public Text notificationText;
 
     private Color gray = new Color32(55, 55, 55, 80);
     private Color green = new Color32(145, 255, 0, 80);
-    private Color red = new Color32(255, 40, 0, 80); 
+    private Color red = new Color32(255, 40, 0, 80);
 
     // Use this for initialization
     void Start()
     {
-        WellDoneCanvas.SetActive(false);
+        if (WellDoneCanvas != null)
+        {
+            WellDoneCanvas.SetActive(false);
+        }
     }
 
     // Update is called once per frame
@@ -29,7 +32,7 @@ public class NotificationController : MonoBehaviour
     public void NotifyText(string text)
     {
         notificationCanvas.enabled = true;
-        backgroundImage.color = gray; 
+        backgroundImage.color = gray;
         notificationText.text = text;
 
         StartCoroutine(CloseAfterSeconds(3));
@@ -39,11 +42,11 @@ public class NotificationController : MonoBehaviour
     {
         if (change > 0)
         {
-            notificationCanvas.enabled = true; 
-            backgroundImage.color = green; 
+            notificationCanvas.enabled = true;
+            backgroundImage.color = green;
             notificationText.text = "Health +" + change;
 
-            StartCoroutine(CloseAfterSeconds(0.5f)); 
+            StartCoroutine(CloseAfterSeconds(0.5f));
         }
 
         else if (change < 0)
